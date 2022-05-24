d631896358a2c1ca99114b9b62af08040a51ea5d
David Glymph <Woozl@users.noreply.github.com>
2022-05-23 21:48:40 -0400

    Initial commit from gatsby

---
+++ gatsby-config.ts
@@ -0,0 +1,11 @@
+import type { GatsbyConfig } from "gatsby";
+
+const config: GatsbyConfig = {
+  siteMetadata: {
+    title: `David Glymph`,
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  plugins: ["gatsby-plugin-sass"]
+};
+
+export default config;
