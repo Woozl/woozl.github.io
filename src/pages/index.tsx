e12bb5eebb455a73bb5aeb55773c2125bb718345
David Glymph <Woozl@users.noreply.github.com>
2022-09-09 23:58:06 -0400

    added icons to homepage and gradient text

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,21 +6,25 @@ import { StaticImage } from 'gatsby-plugin-image';
 import SEO from '../components/seo';
 import { Email } from 'react-obfuscate-email';
 
+import {socials, name} from './index.module.sass';
+import SocialIcon from '../components/SocialIcon';
+
 const Index = ({ location }: PageProps) => {
   return (
     <Layout location={location} pageTitle="Home">
       <SEO title="David Glymph" article={false} />
-      {/* <StaticImage src='https://pbs.twimg.com/media/E1oMV3QVgAIr1NT?format=jpg&name=large' alt='alt desc'></StaticImage> */}
-      <h1>Hello! My name is David Glymph.</h1>
-      <p><em>I'm a front-end developer interested in building performant web apps with an emphasis on maintainable code and thoughtful UX/UI design.</em></p>
-      <p><em>I have experience with React, Typescript, SCSS, GraphQL, and more.</em></p>
+      <h1>Hello! My name is <span className={name}>David Glymph</span>.</h1>
+      <p><em>I'm a full-stack web developer interested in building performant web apps with an emphasis on maintainable code and thoughtful UX/UI design.</em></p>
+      <p><em>I have experience with React, Typescript, SCSS, GraphQL, PostgreSQL and more.</em></p>
       <hr></hr>
-      <h2>Contact me</h2>
-      <ul>
-        <li><Email email="david@davidglymph.com">Email</Email></li>
-        <li><a href="https://github.com/Woozl" target="_blank" rel="noopener noreferrer">Github</a></li>
-        <li><a href="https://linkedin.com/in/davidglymph" target="_blank" rel="noopener noreferrer">LinkedIn</a></li>
-      </ul>
+      <div className={socials}>
+        <h2>Contact me:</h2>
+        <ul>
+          <li><Email email="david@davidglymph.com"><SocialIcon icon='envelope'/>Email</Email></li>
+          <li><a href="https://github.com/Woozl" target="_blank" rel="noopener noreferrer"><SocialIcon icon='github'/>Github</a></li>
+          <li><a href="https://linkedin.com/in/davidglymph" target="_blank" rel="noopener noreferrer"><SocialIcon icon='linkedin'/>LinkedIn</a></li>
+        </ul>
+      </div>
     </Layout>
   );
 };
